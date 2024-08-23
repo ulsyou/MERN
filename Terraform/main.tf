dd581511a00ff1cd97d1cce3d6809b549abfbb2f
Bui Hong Nhat <87702834+ulsyou@users.noreply.github.com>
2024-08-22 23:44:36 -0700

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
+  access_key                  = "test"
+  secret_key                  = "test"
   region                      = "us-east-1"
   s3_use_path_style           = true
   skip_credentials_validation = true
