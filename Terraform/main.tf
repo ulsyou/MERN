f80945e15625e7470f1e6232e621a0715c34395c
Bui Hong Nhat <87702834+ulsyou@users.noreply.github.com>
2024-08-25 01:32:51 -0700

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -32,15 +32,6 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 }
-
-resource "aws_instance" "backend_instance" {
-  ami           = "ami-12345678" 
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
-
-  tags = {
-    Name = "webkidshop-backend"
-  }
   
 resource "aws_instance" "backend_instance" {
   ami           = "ami-0c55b159cbfafe1f0"  
