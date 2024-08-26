ef6b006bd859aa8ba411b462ad0d6df10428f894
Bui Hong Nhat <87702834+ulsyou@users.noreply.github.com>
2024-08-25 21:57:33 -0700

    Update user-data.sh

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 echo "Starting deployment..."
 
-# Clone toàn bộ repository
 git clone https://github.com/ulsyou/MERN.git /home/kali/MERN
 
-# Triển khai Backend
 cd /home/kali/MERN/WebKidShop_BE
 npm install
 nohup npm start > /home/kali/Backend.log 2>&1 &
 
-# Triển khai Frontend
 cd /home/kali/MERN/WebKidShop_FE
 npm install
 npm run build
