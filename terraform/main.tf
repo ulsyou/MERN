ee26d0061ba615212004ea9a9cbc243dafb9eff6
Bui Hong Nhat <87702834+ulsyou@users.noreply.github.com>
2024-08-26 21:20:24 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,9 +81,7 @@ resource "aws_instance" "backend_instance" {
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   tags = {
     Name = "webkidshop-backend"
-  }
-  user_data = base64encode(file("${path.module}/user-data.sh"))
-  
+  }  
   lifecycle {
     create_before_destroy = true
   }
@@ -96,9 +94,7 @@ resource "aws_instance" "frontend_instance" {
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   tags = {
     Name = "webkidshop-frontend"
-  }
-  user_data = base64encode(file("${path.module}/user-data.sh"))
-  
+  }  
   lifecycle {
     create_before_destroy = true
   }
